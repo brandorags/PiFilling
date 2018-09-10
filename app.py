c969a61fe009d0d6749d27a4ba9f4195828d48df
Brandon Ragsdale <brandorags@users.noreply.github.com>
2018-09-10 14:40:25 -0500

    moving bulk of startup code to api/__init__.py

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return 'Hello, World!'
+from api import app
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=app.config['DEBUG'], port=app.config['PORT'])
