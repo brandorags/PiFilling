061ed94186604e3308d56ab17b9f9e78b3bee29e
brandorags <brandorags@users.noreply.github.com>
2018-09-16 17:37:56 -0500

    return PiFillingUser JSON on login

--- api/database/models.py
+++ api/database/models.py
@@ -1,5 +1,6 @@
 from api import db
 
+from typing import Dict
 from flask_login import UserMixin
 
 
@@ -9,3 +10,16 @@ class PiFillingUser(UserMixin, db.Model):
     username = db.Column(db.String, nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean())
+
+    def to_json(self) -> Dict:
+        """
+        Returns a dictionary with keys in the JSON standard
+        camel case format.
+
+        :return: a JSON safe version of PiFillingUser that
+        doesn't contain the user's ID or password hash
+        """
+        return {
+            'username': self.username,
+            'isAdmin': self.is_admin
+        }
