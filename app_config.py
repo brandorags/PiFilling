ba8d4f39df44b0ef48d7efa1e92535988b8d2142
brandorags <brandorags@users.noreply.github.com>
2018-09-15 22:08:18 -0500

    boilerplate for app_config.py

---
+++ app_config.py
@@ -0,0 +1,29 @@
+import os
+from datetime import timedelta
+
+
+# These settings' values may be changed to accommodate where the code will be housed
+# (development vs. production server)
+
+# statement for enabling the development environment
+DEBUG = True
+
+# specify port
+PORT = 5000
+
+# application directory
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# path of log file
+LOG_FILE_LOC = os.path.join(BASE_DIR, 'info.log')
+
+# database location
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'pifilling.db')
+
+# according to SQLAlchemy, this setting adds significant overhead,
+# so don't use it
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# cookie settings
+SESSION_DURATION = timedelta(minutes=15)
+SECRET_KEY = 'thisisaveryverysecretkey'
