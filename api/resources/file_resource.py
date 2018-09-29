d0c1498f128e02cceabc15a8556b132b4243afca
Brandon Ragsdale <brandorags@users.noreply.github.com>
2018-09-28 23:50:04 -0500

    successful save to file system

--- api/resources/file_resource.py
+++ api/resources/file_resource.py
@@ -1,22 +1,25 @@
 import traceback
 
 from flask import request, Blueprint
-from flask_login import login_required, current_user
+from flask_login import login_required
+from api import files_upload_set
 from api.util.http_response_wrapper import ok, internal_server_error
 
 
 file_resource = Blueprint('file_resource', __name__)
 
 
-@file_resource.route('/api/file', methods=['POST'])
+@file_resource.route('/api/file/upload', methods=['POST'])
 @login_required
 def upload_file() -> None:
     try:
         files = request.files
-        for file in files:
-            print(file)
+        for filename in files:
+            file = files[filename]
+            file_path = files_upload_set.save(file)
+            print(file_path)
 
-        return ok({'message': 'File was uploaded!'})
+        return ok({'message': 'File(s) was uploaded!'})
     except Exception as e:
         trace = traceback.format_exc()
         return internal_server_error(e, trace)
