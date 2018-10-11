10eaa0b48748eaff50f037cb2932efbb232643a5
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2018-10-10 23:05:42 -0500

    unit tests for login_resource

--- test/api/resources/login_resource_test.py
+++ test/api/resources/login_resource_test.py
@@ -11,7 +11,7 @@ class LoginResourceTest(PiFillingTest):
     def setUp(self):
         super().setUp()
 
-        username = 'test'
+        username = 'test_user'
         password = 'password'
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256:200000', salt_length=32)
         user = PiFillingUser(username=username, password_hash=hashed_password)
@@ -22,13 +22,29 @@ class LoginResourceTest(PiFillingTest):
     def tearDown(self):
         super().tearDown()
 
-    def test_user_added(self):
-        user = PiFillingUser.query.filter_by(username='test').first()
-        self.assertEqual(user.username, 'test', 'username should be ' + user.username)
+    def test_login_success(self):
+        user = PiFillingUser.query.filter_by(username='test_user').first()
 
-    # def test_login(self):
-    #     response = self.client.get('/api/login')
-    #     self.assertEqual(response.json, dict(success=True))
+        data = self.client.post('/api/login', json={
+            'username': 'test_user', 'password': 'password'
+        })
+        user_json = data.get_json()
+
+        self.assertEqual(user_json, user.to_json())
+
+    def test_login_unauthorized(self):
+        data = self.client.post('/api/login', json={
+            'username': 'fake_user', 'password': 'password'
+        })
+
+        self.assert401(data)
+
+    def test_login_error(self):
+        data = self.client.post('/api/login', json={
+            'wrongKey': 'fake_user', 'password': 'password'
+        })
+
+        self.assert500(data)
 
 
 if __name__ == '__main__':
