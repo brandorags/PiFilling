9d1efa0e8ca540da5e80b676f5d0a9f0d1b469a8
brandorags <brandorags@users.noreply.github.com>
2018-10-22 22:56:32 -0500

    get file metadata for given path

--- api/resources/file_resource.py
+++ api/resources/file_resource.py
@@ -1,4 +1,5 @@
 import traceback
+import os
 
 from typing import List
 from flask import request, Blueprint
@@ -8,10 +9,23 @@ from api.util.http_response_wrapper import ok, internal_server_error
 from api.database.models import FileMetadata
 
 
-file_resource = Blueprint('file_resource', __name__)
+file_resource = Blueprint('file_resource', __name__, url_prefix='/api/file')
 
 
-@file_resource.route('/api/file/upload', methods=['POST'])
+@file_resource.route('', methods=['GET'])
+@login_required
+def get_file_metadata_list_for_path() -> List[FileMetadata]:
+    try:
+        path = request.args.get('path')
+        file_metadata_list = FileMetadata.query.filter_by(path=path).all()
+
+        return ok([metadata.to_json() for metadata in file_metadata_list])
+    except Exception as e:
+        trace = traceback.format_exc()
+        return internal_server_error(e, trace)
+
+
+@file_resource.route('upload', methods=['POST'])
 @login_required
 def upload_file() -> List[FileMetadata]:
     try:
@@ -22,13 +36,17 @@ def upload_file() -> List[FileMetadata]:
 
         for f in files_to_upload:
             file = files_to_upload[f]
-            result = files_upload_set.save(file, user.username)
+            filename_with_path = files_upload_set.save(file, user.username)
 
-            split_result = result.rsplit('/', 1)
-            path = split_result[0]
-            filename = split_result[1]
+            split_filename_with_path = filename_with_path.rsplit('/', 1)
+            relative_file_path = split_filename_with_path[0]
+            filename = split_filename_with_path[1]
 
-            saved_file = FileMetadata(user_id=user.id, path=path, filename=filename)
+            absolute_file_path = files_upload_set.path(filename_with_path)
+            file_size = os.stat(absolute_file_path).st_size
+
+            saved_file = FileMetadata(user_id=user.id, path=relative_file_path,
+                                      filename=filename, file_size=file_size)
             saved_files.append(saved_file)
 
         if saved_files:
