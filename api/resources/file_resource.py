24e8ec0b28b721ea80fc265f9800a932f8bdd484
brandorags <brandorags@users.noreply.github.com>
2018-12-05 18:28:40 -0600

    edits to upload and create_new_directory

--- api/resources/file_resource.py
+++ api/resources/file_resource.py
@@ -1,13 +1,12 @@
 import traceback
-import os
 
 from typing import List
 from subprocess import run, SubprocessError
 from flask import request, Blueprint
 from flask_login import login_required, current_user
-from api import app, files_upload_set, db
+from api import app, files_upload_set
 from api.util.http_response_wrapper import ok, internal_server_error
-from api.database.db_models import FileMetadata
+from api.models.file_metadata import FileMetadata
 from api.util.directory_content_parser import DirectoryContentParser
 
 
@@ -32,30 +31,18 @@ def get_file_metadata_list_for_path() -> List[FileMetadata]:
 def upload() -> List[FileMetadata]:
     try:
         files_to_upload = request.files
-        saved_files = []
-
-        user = current_user
+        path = current_user.username
 
         for f in files_to_upload:
             file = files_to_upload[f]
-            filename_with_path = files_upload_set.save(file, user.username)
+            files_upload_set.save(file, path)
 
-            split_filename_with_path = filename_with_path.rsplit('/', 1)
-            relative_file_path = split_filename_with_path[0]
-            filename = split_filename_with_path[1]
+        file_metadata_list = []
+        if files_to_upload:
+            absolute_path = files_upload_set.config.destination + '/' + path
+            file_metadata_list = DirectoryContentParser.parse_directory_content(absolute_path)
 
-            absolute_file_path = files_upload_set.path(filename_with_path)
-            file_size = os.stat(absolute_file_path).st_size
-
-            saved_file = FileMetadata(user_id=user.id, path=relative_file_path,
-                                      filename=filename, file_size=file_size)
-            saved_files.append(saved_file)
-
-        if saved_files:
-            db.session.bulk_save_objects(saved_files)
-            db.session.commit()
-
-        return ok([file.to_json() for file in saved_files])
+        return ok([metadata.to_json() for metadata in file_metadata_list])
     except Exception as e:
         trace = traceback.format_exc()
         return internal_server_error(e, trace)
@@ -68,9 +55,10 @@ def create_new_directory() -> [str]:
         data = request.get_json()
         directory_name = data['name']
         path = data['path']
-        base_path = app.config['UPLOADED_FILES_DEST']
 
-        create_new_directory_proc = run(['mkdir', base_path + path + directory_name], capture_output=True)
+        absolute_path = files_upload_set.config.destination + '/' + path + directory_name
+
+        create_new_directory_proc = run(['mkdir', '-p', absolute_path], capture_output=True)
         if create_new_directory_proc.returncode != 0:
             raise SubprocessError(create_new_directory_proc.stderr)
 
