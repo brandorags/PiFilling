befc613e822cd4ea4c4ee4505a07b714fefd4d2f
Brandon Ragsdale <brandorags@users.noreply.github.com>
2018-12-09 21:43:34 -0700

    renamed db_models to entities

--- app/api/login_resource.py
+++ app/api/login_resource.py
@@ -21,7 +21,7 @@ import traceback
 from flask import request, Blueprint
 from flask_login import login_user
 from werkzeug.security import check_password_hash
-from app.database.db_models import PiFillingUser
+from app.database.entities import PiFillingUser
 from app.util.http_response_wrapper import ok, unauthorized, internal_server_error
 
 
