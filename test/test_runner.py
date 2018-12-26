c56f1614b2975c5c373122dc341e0070bc095113
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2018-12-25 23:36:02 -0600

    load test results into one file

--- test/test_runner.py
+++ test/test_runner.py
@@ -2,12 +2,18 @@ import unittest
 import os
 import xmlrunner
 
+from pathlib import Path
+
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
     start_dir = os.path.abspath(os.path.dirname(__file__))
     test_suite = loader.discover(start_dir=start_dir, pattern='*test.py')
 
-    output_dir = './test-reports/unittest'
-    runner = xmlrunner.XMLTestRunner(output=output_dir)
-    runner.run(test_suite)
+    results_dir = Path(start_dir + '/test-reports/unittest/')
+    if not results_dir.is_dir():
+        os.makedirs(results_dir.as_posix())
+
+    with open(results_dir.as_posix() + 'results.xml', 'wb') as results_file:
+        runner = xmlrunner.XMLTestRunner(output=results_file)
+        runner.run(test_suite)
