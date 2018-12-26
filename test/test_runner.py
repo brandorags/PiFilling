cc213f56782245ba507ce0431ee58d1446e2ca36
Brandon Ragsdale <brandon.s.ragsdale@gmail.com>
2018-12-25 22:59:07 -0600

    enable reporting in CircleCI with unittest-xml-reporting

--- test/test_runner.py
+++ test/test_runner.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import xmlrunner
 
 
 if __name__ == '__main__':
@@ -7,5 +8,6 @@ if __name__ == '__main__':
     start_dir = os.path.abspath(os.path.dirname(__file__))
     test_suite = loader.discover(start_dir=start_dir, pattern='*test.py')
 
-    runner = unittest.TextTestRunner()
+    output_dir = './test-reports/unittest'
+    runner = xmlrunner.XMLTestRunner(output=output_dir)
     runner.run(test_suite)
