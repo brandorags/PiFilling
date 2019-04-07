f610a811b895b1ee2b5d2b861d8c1079decfb149
Brandon Ragsdale <brandorags@users.noreply.github.com>
2019-04-07 00:27:00 -0500

    new route for returning only directories for a given path

--- test/util/directory_content_parser_test.py
+++ test/util/directory_content_parser_test.py
@@ -14,32 +14,43 @@
 
 
 import unittest
-import subprocess
+import shutil
+import os
 
 from app.util.directory_content_parser import DirectoryContentParser
 from app.models.file_metadata import FileMetadata
+from app.models.directory_path import DirectoryPath
+from test.pifilling_test import PiFillingTest
 
 
-class DirectoryContentParserTest(unittest.TestCase):
+class DirectoryContentParserTest(PiFillingTest):
 
     def setUp(self):
-        temp_dir = 'temp_directory'
-        subprocess.run(['mkdir', temp_dir])
+        super().setUp()
+
+        base_dir = self.app.config['UPLOADED_FILES_DEST']
+        os.mkdir(base_dir)
+
+        temp_dir = base_dir + '/temp_directory'
+        os.mkdir(temp_dir)
 
         for i in range(0, 5):
             with open(temp_dir + '/temp' + str(i) + '.txt', 'w') as file:
                 file.write('Test test test')
 
-        subprocess.run(['mkdir', temp_dir + '/temp_directory1'])
+        os.mkdir(temp_dir + '/temp_directory1')
+        os.mkdir(temp_dir + '/temp_directory2')
 
     def tearDown(self):
-        subprocess.run(['rm', '-rf', 'temp_directory'])
+        super().tearDown()
+
+        shutil.rmtree(self.app.config['UPLOADED_FILES_DEST'])
 
     def test_parse_directory_content(self):
-        file_metadata_list = DirectoryContentParser.parse_directory_content('./temp_directory')
+        file_metadata_list = DirectoryContentParser.parse_directory_content('temp_directory')
         file_metadata_first_item = file_metadata_list[0]
 
-        self.assertEqual(len(file_metadata_list), 6)
+        self.assertEqual(len(file_metadata_list), 7)
         self.assertTrue(isinstance(file_metadata_first_item, FileMetadata))
         self.assertEqual(file_metadata_first_item.filename, 'temp0.txt')
         self.assertEqual(file_metadata_first_item.file_type, '.txt')
@@ -47,8 +58,17 @@ class DirectoryContentParserTest(unittest.TestCase):
         self.assertIsNotNone(file_metadata_first_item.file_size)
         self.assertFalse(file_metadata_first_item.is_directory)
 
+    def test_get_directory_list(self):
+        directory_list = DirectoryContentParser.get_directory_list('temp_directory')
+        directory_list_first_item = directory_list[0]
+
+        self.assertEqual(len(directory_list), 2)
+        self.assertTrue(isinstance(directory_list_first_item, DirectoryPath))
+        self.assertEqual(directory_list_first_item.directory_name, 'temp_directory1')
+        self.assertEqual(directory_list_first_item.path, 'temp_directory')
+
     def test_get_file(self):
-        file_metadata = DirectoryContentParser.get_file('./temp_directory/temp0.txt')
+        file_metadata = DirectoryContentParser.get_file('temp_directory/temp0.txt')
 
         self.assertTrue(isinstance(file_metadata, FileMetadata))
         self.assertEqual(file_metadata.filename, 'temp0.txt')
